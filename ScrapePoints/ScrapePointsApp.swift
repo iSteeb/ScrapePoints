540ca09d591a6d1bae1a13c1e8a1fa2b7046df5a
Steven Duzevich <steven@duz.ie>
2023-11-17 12:04:50 +0100

    Initial Commit

---
+++ ScrapePoints/ScrapePointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrapePointsApp.swift
+//  ScrapePoints
+//
+//  Created by Steven Duzevich on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrapePointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
